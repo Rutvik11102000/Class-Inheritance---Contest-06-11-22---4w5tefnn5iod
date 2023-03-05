690cec289e412e0f3b94e198c30adcb7be0bd019
Rutvik11102000 <101519046+Rutvik11102000@users.noreply.github.com>
2023-03-05 21:25:01 +0530

    Assignment

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,17 @@
 
 
 // export class using module.exports
+const Shape = require("./shape");
+
+class Circle extends Shape {
+  constructor(color, radius) {
+    super(color);
+    this.radius = radius;
+  }
+
+  calculateArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+module.exports = Circle;
